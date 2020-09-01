979933ce0f331ed2e38b758e8bdbd0d09e16468e
sundaydavid <sundaydavid989@gmail.com>
2020-09-01 23:10:16 +0100

    finished setting up viewpager

--- app/src/main/java/com/sundaydavid989/aad440assignment/MainActivity.kt
+++ app/src/main/java/com/sundaydavid989/aad440assignment/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.sundaydavid989.aad440assignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sundaydavid989.aad440assignment.databinding.ActivityMainBinding
+import com.sundaydavid989.aad440assignment.ui.adapter.ViewPagerAdapter
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        binding.viewPager.adapter = adapter
+        setContentView(view)
     }
 }
