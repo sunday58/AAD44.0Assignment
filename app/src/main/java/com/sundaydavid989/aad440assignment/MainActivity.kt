c1dfb9136e995cd10190e1802335f4bf2586f1a6
sundaydavid <sundaydavid989@gmail.com>
2020-09-02 21:19:40 +0100

    fixed pager tabs in same page

--- app/src/main/java/com/sundaydavid989/aad440assignment/MainActivity.kt
+++ app/src/main/java/com/sundaydavid989/aad440assignment/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.sundaydavid989.aad440assignment
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sundaydavid989.aad440assignment.databinding.ActivityMainBinding
 import com.sundaydavid989.aad440assignment.ui.adapter.ViewPagerAdapter
+import com.sundaydavid989.aad440assignment.ui.submit.SubmitActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +19,12 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
 
         val adapter = ViewPagerAdapter(supportFragmentManager)
-        binding.pagerHeader.setTextColor(Color.WHITE)
-        binding.pagerHeader.setTabIndicatorColorResource(R.color.white)
         binding.viewPager.adapter = adapter
+        binding.pagerHeader.setViewPager(binding.viewPager)
+
+        binding.submitButton.setOnClickListener {
+            startActivity(Intent(this, SubmitActivity::class.java))
+        }
         setContentView(view)
     }
 }
---
+++ app/src/main/java/com/sundaydavid989/aad440assignment/ui/submit/SubmitActivity.kt
@@ -0,0 +1,12 @@
+package com.sundaydavid989.aad440assignment.ui.submit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sundaydavid989.aad440assignment.R
+
+class SubmitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit)
+    }
+}
