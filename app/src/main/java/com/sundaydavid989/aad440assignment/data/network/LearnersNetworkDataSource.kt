c1778c23d8d551a716360d3581e9aa179ec30bf8
sundaydavid <sundaydavid989@gmail.com>
2020-09-03 02:37:31 +0100

    implemented api call

--- app/src/main/java/com/sundaydavid989/aad440assignment/data/network/LearnersNetworkDataSource.kt
+++ app/src/main/java/com/sundaydavid989/aad440assignment/data/network/LearnersNetworkDataSource.kt
@@ -1,4 +1,10 @@
 package com.sundaydavid989.aad440assignment.data.network
 
+import androidx.lifecycle.LiveData
+import com.sundaydavid989.aad440assignment.data.entity.Hours
+
 interface LearnersNetworkDataSource {
+    val downloadLearnersHours: LiveData<Hours>
+
+    suspend fun fetchHours()
 }
---
+++ app/src/main/java/com/sundaydavid989/aad440assignment/data/network/LearnersNetworkDataSourceImpl.kt
@@ -0,0 +1,27 @@
+package com.sundaydavid989.aad440assignment.data.network
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.sundaydavid989.aad440assignment.data.entity.Hours
+import com.sundaydavid989.aad440assignment.internal.NoConnectivityException
+
+class LearnersNetworkDataSourceImpl(
+    private val learnersApiService: LearnersApiService
+) : LearnersNetworkDataSource {
+    private val _downloadLearnersHours = MutableLiveData<Hours>()
+    override val downloadLearnersHours: LiveData<Hours>
+        get() = _downloadLearnersHours
+
+    override suspend fun fetchHours() {
+        try {
+            val fetchHours = learnersApiService
+                .getHoursAsync()
+                .await()
+            _downloadLearnersHours.postValue(fetchHours)
+        }
+        catch (e: NoConnectivityException){
+            Log.d("connectivity ", "No internet connection")
+        }
+    }
+}
