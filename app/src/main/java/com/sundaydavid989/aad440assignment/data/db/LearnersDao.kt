1f97897729bf6532c4bc11cf634c69ecdb3b2f77
sundaydavid <sundaydavid989@gmail.com>
2020-09-03 02:42:46 +0100

    added room dao

--- app/src/main/java/com/sundaydavid989/aad440assignment/data/db/LearnersDao.kt
+++ app/src/main/java/com/sundaydavid989/aad440assignment/data/db/LearnersDao.kt
@@ -1,4 +1,18 @@
 package com.sundaydavid989.aad440assignment.data.db
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.sundaydavid989.aad440assignment.data.entity.Hours
+
+@Dao
 interface LearnersDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upSert(hoursItem: Hours)
+
+    @Query("SELECT * FROM learners_hours")
+    fun getHours(): LiveData<Hours>
 }
--- app/src/main/java/com/sundaydavid989/aad440assignment/data/entity/HoursItem.kt
+++ app/src/main/java/com/sundaydavid989/aad440assignment/data/entity/HoursItem.kt
@@ -1,7 +1,12 @@
 package com.sundaydavid989.aad440assignment.data.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+
+@Entity(tableName = "learners_hours")
 data class HoursItem(
+    @PrimaryKey(autoGenerate = true)
     val badgeUrl: String,
     val country: String,
     val hours: Int,
