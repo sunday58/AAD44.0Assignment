dae1097a440b76f1e303ef09bca03868e3254e21
sundaydavid <sundaydavid989@gmail.com>
2020-09-05 23:37:52 +0100

    making submit call

--- app/src/main/java/com/sundaydavid989/aad440assignment/Constants.kt
+++ app/src/main/java/com/sundaydavid989/aad440assignment/Constants.kt
@@ -2,4 +2,5 @@ package com.sundaydavid989.aad440assignment
 
 object Constants {
     const val URL = "https://gadsapi.herokuapp.com/"
+    const val SUBMIT_URL = "https://docs.google.com/forms/d/e/"
 }
--- app/src/main/java/com/sundaydavid989/aad440assignment/data/network/SubmitApiService.kt
+++ app/src/main/java/com/sundaydavid989/aad440assignment/data/network/SubmitApiService.kt
@@ -7,42 +7,36 @@ import com.sundaydavid989.aad440assignment.data.entity.SkillItem
 import kotlinx.coroutines.Deferred
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface SubmitApiService {
 
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    @FormUrlEncoded
+    fun submitAssignment(
+        @Field("entry.1877115667")firstName: String,
+        @Field("entry.2006916086")lastName: String,
+        @Field("entry.1824927963")emailAddress: String,
+        @Field("entry.284483984")linkToProject: String
+    ): Call<Void>
 
+  companion object ServiceBuilder {
+       private val client = OkHttpClient.Builder().build()
 
-    companion object {
-        operator fun invoke(
-            connectivityInterceptor: ConnectivityInterceptor
-        ): SubmitApiService {
-            val requestInterceptor = Interceptor{chain ->
-                val url = chain.request()
-                    .url()
-                    .newBuilder()
-                    .build()
-                val request = chain.request()
-                    .newBuilder()
-                    .url(url)
-                    .build()
+       private val retrofit = Retrofit.Builder()
+           .baseUrl(Constants.SUBMIT_URL)
+           .addConverterFactory(GsonConverterFactory.create())
+           .client(client)
+           .build()
 
-                return@Interceptor chain.proceed(request)
-            }
-            val okHttpClient = OkHttpClient.Builder()
-                .addInterceptor(requestInterceptor)
-                .addInterceptor(connectivityInterceptor)
-                .build()
-
-            return Retrofit.Builder()
-                .client(okHttpClient)
-                .baseUrl(Constants.URL)
-                .addCallAdapterFactory(CoroutineCallAdapterFactory())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(SubmitApiService::class.java)
-        }
-    }
+       fun <T> buildService(service: Class<T>): T{
+           return retrofit.create(service)
+       }
+   }
 }
